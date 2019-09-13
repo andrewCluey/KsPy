c354d5bc57af8e53f9a94536dd5a754bece90078
aclure <aclure@globalprocessing.net>
2019-09-13 10:47:23 +0100

    removed pw.py

--- kspy/pw.py
+++
@@ -1,7 +0,0 @@
-import getpass
-from passlib.hash import sha512_crypt
-
-
-pw = getpass.getpass(prompt='Enter the proposed esxi password: ')
-hash = sha512_crypt.encrypt(pw, rounds=5000)
-print(hash)
